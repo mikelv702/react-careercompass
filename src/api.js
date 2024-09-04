b71ecc7ff53c13dbeddb2616fb5040729c2aa024
Michael Hall <michael@rubberduck-labs.com>
2024-09-04 00:10:52 +0000

    Changing API to be enviroment variable for deployments

--- src/api.js
+++ src/api.js
@@ -1,4 +1,5 @@
-const API_URL = 'http://docker03.lab.engineerhub.xyz:8001'; // Replace with your actual API URL
+// const API_URL = 'http://docker03.lab.engineerhub.xyz:8001'; // Replace with your actual API URL
+const API_URL = process.env.REACT_APP_API_URL;
 
 export const loginUser = async (username, password) => {
   const response = await fetch(`${API_URL}/token`, {
@@ -12,12 +13,12 @@ export const loginUser = async (username, password) => {
   if (!response.ok) {
     throw new Error('Login failed');
   }
-
+  console.log(`${API_URL}`)
   return response.json();
 };
 
 export const getTasks = async (token) => {
-  const response = await fetch(`${API_URL}/task/`, {
+  const response = await fetch(`${API_URL}/task`, {
     headers: {
       'Authorization': `Bearer ${token}`,
     },
@@ -26,12 +27,12 @@ export const getTasks = async (token) => {
   if (!response.ok) {
     throw new Error('Failed to fetch tasks');
   }
-
+  console.log(`${API_URL}`)
   return response.json();
 };
 
 export const createTask = async (token, description) => {
-  const response = await fetch(`${API_URL}/task/`, {
+  const response = await fetch(`${API_URL}/task`, {
     method: 'POST',
     headers: {
       'Authorization': `Bearer ${token}`,
@@ -43,6 +44,6 @@ export const createTask = async (token, description) => {
   if (!response.ok) {
     throw new Error('Failed to create task');
   }
-
+  console.log(`${API_URL}`) 
   return response.json();
 };
